3ac12e1e2b5013ef22e02750377fb04f2e61f049
ThrDev <zacharyb@clientcommand.com>
2021-01-15 09:14:15 -0500

    Open URL without try/catch

--- src/Shotr.Core/Extensions.cs
+++ src/Shotr.Core/Extensions.cs
@@ -7,30 +7,17 @@ namespace Shotr.Core
     {
         public static void OpenUrl(this string url)
         {
-            try
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                Process.Start(url);
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
             }
-            catch
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    url = url.Replace("&", "^&");
-                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    Process.Start("xdg-open", url);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    Process.Start("open", url);
-                }
-                else
-                {
-                    throw;
-                }
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
             }
         }
     }
